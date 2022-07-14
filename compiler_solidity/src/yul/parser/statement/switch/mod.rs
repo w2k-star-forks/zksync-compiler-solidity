215b617ac87485366ee3e5a6d1573468cfb38458
Alex Z <hedgar2017@gmail.com>
2022-07-14 09:42:04 +0200

    Bring back the LLVM IR switch for Yul (#13)
    * Bring back the LLVM IR switch for Yul
    
    * Update deps
    
    * Resolve conflicts
    
    * Bump the LLVM target version
    
    Co-authored-by: Alex Z <a.zarudnyy@matterlabs.dev>

--- compiler_solidity/src/yul/parser/statement/switch/mod.rs
+++ compiler_solidity/src/yul/parser/statement/switch/mod.rs
@@ -97,43 +97,44 @@ where
             return Ok(());
         }
 
-        let join_block = context.append_basic_block("switch_join");
+        let current_block = context.basic_block();
+        let join_block = context.append_basic_block("switch_join_block");
 
-        let mut current_block = context.append_basic_block("switch_case_constant_1");
-        context.build_unconditional_branch(current_block);
-
+        let mut branches = Vec::with_capacity(self.cases.len());
         for (index, case) in self.cases.into_iter().enumerate() {
-            let expression_block =
-                context.append_basic_block(format!("switch_case_branch_{}", index + 1).as_str());
+            let constant = case.literal.into_llvm(context).to_llvm();
+
+            let expression_block = context
+                .append_basic_block(format!("switch_case_branch_{}_block", index + 1).as_str());
             context.set_basic_block(expression_block);
             case.block.into_llvm(context)?;
             context.build_unconditional_branch(join_block);
 
-            context.set_basic_block(current_block);
-            let scrutinee = self
-                .expression
-                .clone()
-                .into_llvm(context)?
-                .expect("Always exists");
-            let constant = case.literal.into_llvm(context);
-            let comparison = context.builder().build_int_compare(
-                inkwell::IntPredicate::EQ,
-                constant.to_llvm().into_int_value(),
-                scrutinee.to_llvm().into_int_value(),
-                format!("switch_case_condition_{}", index).as_str(),
-            );
-
-            let next_block =
-                context.append_basic_block(format!("switch_case_constant_{}", index + 2).as_str());
-            context.build_conditional_branch(comparison, expression_block, next_block);
-            current_block = next_block;
+            branches.push((constant.into_int_value(), expression_block));
         }
+
+        let default_block = match self.default {
+            Some(default) => {
+                let default_block = context.append_basic_block("switch_default_block");
+                context.set_basic_block(default_block);
+                default.into_llvm(context)?;
+                context.build_unconditional_branch(join_block);
+                default_block
+            }
+            None => join_block,
+        };
 
         context.set_basic_block(current_block);
-        if let Some(block) = self.default {
-            block.into_llvm(context)?;
-        }
-        context.build_unconditional_branch(join_block);
+        let scrutinee = self
+            .expression
+            .into_llvm(context)?
+            .expect("Always exists")
+            .to_llvm();
+        context.builder().build_switch(
+            scrutinee.into_int_value(),
+            default_block,
+            branches.as_slice(),
+        );
 
         context.set_basic_block(join_block);
 
